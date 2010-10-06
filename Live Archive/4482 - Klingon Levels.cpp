3de085559531150ce83b29de505ac0f980f9c14a
marioyc <marioyc@marioyc-laptop.(none)>
2010-10-06 12:13:07 -0500

    Live Archive 4482 - Klingon Levels

--- Live Archive/4482 - Klingon Levels.cpp
+++ Live Archive/4482 - Klingon Levels.cpp
@@ -1,4 +1,5 @@
 #include <cstdio>
+#include <cstring>
 #include <algorithm>
 
 using namespace std;
@@ -10,32 +11,32 @@ int main(){
         scanf("%d",&N);
         if(N==0) break;
         
-        fill(aux,aux+1001,0);
+        memset(aux,0,sizeof(aux));
         
-        mx=ans=0;
+        ans = 0;
         
-        for(int i=0;i<N;i++){
+        for(int i = 0;i<N;++i){
             scanf("%d",&K);
-            ans+=K;
+            ans += K;
             
-            fill(sum,sum+1001,0);
+            memset(sum,0,sizeof(sum));
             
-            for(int j=K-1;j>=0;j--){
+            for(int j = K-1;j>=0;--j){
                 scanf("%d",&x);
-                sum[x]++;
+                ++sum[x];
             }
             
-            for(int i=999;i>=0;i--) sum[i]+=sum[i+1];
+            for(int i = 999;i>=0;--i) sum[i] += sum[i+1];
             
-            for(int i=0;i<=1000;i++){
-                a=sum[i];
-                b=K-a;
-                aux[i]+=abs(a-b);
+            for(int i = 0;i<=1000;++i){
+                a = sum[i];
+                b = K-a;
+                aux[i] += abs(a-b);
             }
         }
         
-        for(int j=0;j<=1000;j++)
-            if(aux[j]<ans) ans=aux[j];
+        for(int j = 0;j<=1000;++j)
+            if(aux[j]<ans) ans = aux[j];
         
         printf("%d\n",ans);
     }
