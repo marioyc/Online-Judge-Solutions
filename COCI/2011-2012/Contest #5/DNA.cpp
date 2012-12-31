828a1486abf6028edc2a3789163a11dab4cddd87
marioyc <ycmario@gmail.com>
2012-12-31 17:47:52 -0500

    COCI 2011-2012 Contest #5

--- COCI/2011-2012/Contest #5/DNA.cpp
+++ COCI/2011-2012/Contest #5/DNA.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 #define MAXN 1000000
 
-int dp[MAXN][2];
+int dp[2][2];
 char s[MAXN + 1];
 
 int main(){
@@ -21,12 +21,12 @@ int main(){
         dp[0][1] = 1;
     }
     
-    for(int i = 1;i < N;++i){
-        dp[i][0] = (s[i] == 'A'? 0 : 1) + min(dp[i - 1][0],dp[i - 1][1]);
-        dp[i][1] = (s[i] == 'B'? 0 : 1) + min(dp[i - 1][0] + 2,dp[i - 1][1]);
+    for(int i = 1,r = 1;i < N;++i,r ^= 1){
+        dp[r][0] = (s[i] == 'A'? 0 : 1) + min(dp[r ^ 1][0],dp[r ^ 1][1]);
+        dp[r][1] = (s[i] == 'B'? 0 : 1) + min(dp[r ^ 1][0] + 2,dp[r ^ 1][1]);
     }
     
-    printf("%d\n",min(dp[N - 1][0],dp[N - 1][1]));
+    printf("%d\n",min(dp[(N - 1) & 1][0],dp[(N - 1) & 1][1]));
     
     return 0;
 }
